612ef63171a12187474a0b4952636316f965d8bb
Kate <52196833+kate-49@users.noreply.github.com>
2020-05-14 08:37:43 +0100

    Add files via upload

---
+++ app.rb
@@ -0,0 +1,35 @@
+require 'sinatra/base'
+require './lib/player'
+require './lib/game'
+
+class Battle < Sinatra::Base
+  enable :sessions
+
+  get '/' do
+    erb :index
+  end
+
+  post '/names' do
+    player_1 = Player.new(params[:player_1_name])
+    player_2 = Player.new(params[:player_2_name])
+    $game = Game.new(player_1, player_2)
+    redirect '/play'
+  end
+
+  get '/play' do
+    @game = $game
+    erb :play
+  end
+
+  get '/attack' do
+    @game = $game
+    @game.attack(@game.player_2)
+    erb :attack
+  end
+
+  post '/switch-turns' do
+    $game.switch_turns
+    redirect('/play')
+  end
+  run! if app_file == $0
+end
---
+++ attack_spec.rb
@@ -0,0 +1,23 @@
+feature 'Attacking' do
+  scenario 'attack Player 2' do
+    sign_in_and_play
+    click_button 'Attack'
+    expect(page).to have_content 'Dave attacked Mittens'
+  end
+
+  scenario 'reduce Player 2 HP by 10' do
+    sign_in_and_play
+    click_button 'Attack'
+    click_button 'OK'
+    expect(page).not_to have_content 'Mittens: 60 HP'
+    expect(page).to have_content 'Mittens: 50HP'
+  end
+
+  scenario 'be attacked by Player 2' do
+    sign_in_and_play
+    click_button 'Attack'
+    click_button 'OK'
+    click_button 'Attack'
+    expect(page).to have_content 'Mittens attacked Dave'
+  end
+end
